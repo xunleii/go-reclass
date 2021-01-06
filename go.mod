3a5ce8c3588c90062aa9d2745603fc0670ae3a48
Alexandre Nicolaie <alexandre.nicolaie@gmail.com>
2021-01-06 22:32:31 +0100

    feat(tests): implements model tests in Go

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/xunleii/go-reclass
+
+go 1.14
+
+require (
+	github.com/kyoh86/richgo v0.3.6 // indirect
+	github.com/kyoh86/xdg v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/stretchr/testify v1.6.1
+	github.com/wacul/ptr v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20210105210732-16f7687f5001 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210106172901-c476de37821d
+)
---
+++ go_reclass_model_test.go
@@ -0,0 +1,52 @@
+package goreclass
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"gopkg.in/yaml.v3"
+)
+
+type ModelTestSuite struct{ suite.Suite }
+
+func (s *ModelTestSuite) TestDefault()                { s.compareFromYaml("default") }
+func (s *ModelTestSuite) TestFeaturedSimple()         { s.compareFromYaml("featured/simple") }
+func (s *ModelTestSuite) TestFeaturedWithClasses()    { s.compareFromYaml("featured/with-classes") }
+func (s *ModelTestSuite) TestFeaturedWithNestedRefs() { s.compareFromYaml("featured/with-nested-refs") }
+func (s *ModelTestSuite) TestFeaturedWithOverrides()  { s.compareFromYaml("featured/with-overrides") }
+func (s *ModelTestSuite) TestFeaturedWithRefs()       { s.compareFromYaml("featured/with-refs") }
+func (s *ModelTestSuite) TestFeaturedWithRelName()    { s.compareFromYaml("featured/with-relative-name") }
+func (s *ModelTestSuite) TestExtendedWithClassLoop()  { s.compareFromYaml("extended/with-class-loop") }
+func (s *ModelTestSuite) TestExtendedWithRefInClass() { s.compareFromYaml("extended/with-ref-in-class") }
+
+func (s *ModelTestSuite) TestErroredOverrideConstant() {
+	reclassFirstNodePath := path.Join("test/model/errored/override-constant/classes/first.yml")
+	_, err := BuildInventory(reclassFirstNodePath)
+
+	s.Error(err, "BuildInventory must fail if a constant is overrode.")
+}
+
+func TestModelSuiteTest(t *testing.T) { suite.Run(t, new(ModelTestSuite)) }
+func (s *ModelTestSuite) compareFromYaml(modelName string) {
+	reclassYamlPath := path.Join("test/model", modelName+".reclassed.yml")
+	reclassYaml, err := ioutil.ReadFile(reclassYamlPath)
+	if err != nil {
+		s.FailNow(fmt.Sprintf("failed to read `reclassed` YAML file %s: %s", reclassYamlPath, err))
+	}
+
+	var reclassInventory Inventory
+	if err := yaml.Unmarshal(reclassYaml, &reclassInventory); err != nil {
+		s.FailNow(fmt.Sprintf("failed to unmarshal `reclassed` YAML file %s: %s", reclassYamlPath, err))
+	}
+
+	reclassFirstNodePath := path.Join("test/model", modelName, "classes/first.yml")
+	goreclassInventory, err := BuildInventory(reclassFirstNodePath)
+	if err := yaml.Unmarshal(reclassYaml, &reclassInventory); err != nil {
+		s.FailNow(fmt.Sprintf("failed to build inventory from %s: %s", path.Join("test/model", modelName), err))
+	}
+
+	s.Equalf(&reclassInventory, goreclassInventory, "Inventories differs for model `%s`.", modelName)
+}
